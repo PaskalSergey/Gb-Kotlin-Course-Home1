34142abd285227c1389ea168b5959f961758d9f9
PaskalSergey <paskal.sergey@gmail.com>
2022-08-19 23:17:13 +0300

    проверил permission на чтение контактов

--- app/src/main/java/gb/kotlin_course_home_1/view/contentprovider/ContentProviderFragment.kt
+++ app/src/main/java/gb/kotlin_course_home_1/view/contentprovider/ContentProviderFragment.kt
@@ -1,9 +1,15 @@
 package gb.kotlin_course_home_1.view.contentprovider
 
+import android.Manifest
+import android.content.ContentResolver
+import android.content.pm.PackageManager
+import android.database.Cursor
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import gb.kotlin_course_home_1.databinding.FragmentContentProviderBinding
 
@@ -25,6 +31,22 @@ class ContentProviderFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    fun checkPermission() {
+        val permResult =
+            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_CONTACTS)
+        PackageManager.PERMISSION_GRANTED
+        if(permResult == PackageManager.PERMISSION_GRANTED){
+            getContacts()
+        }
+    }
+
+    private fun getContacts() {
+        val contentResolver: ContentResolver = requireContext().contentResolver
+        val cursorWithContacts: Cursor? =
+            contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null)
 
     }
 
